4c382aed1791157c70dbc92f80630c5d6d629270
Roziiii <poprosian@gmail.com>
2020-04-06 04:23:26 -0700

    Initial commit

---
+++ src/main/java/com/rcldevelop/moremekanismores/MoreMekanismOres.java
@@ -0,0 +1,38 @@
+package com.rcldevelop.moremekanismores;
+
+import com.rcldevelop.moremekanismores.util.RegistryHandler;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+
+@Mod("mmko")
+public class MoreMekanismOres
+{
+
+    private static final Logger LOGGER = LogManager.getLogger();
+    public static final String MOD_ID="mmko";
+
+    public MoreMekanismOres() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+
+        RegistryHandler.init();
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+
+    }
+
+    private void doClientStuff(final FMLClientSetupEvent event) {
+
+    }
+
+}
---
+++ src/main/java/com/rcldevelop/moremekanismores/util/RegistryHandler.java
@@ -0,0 +1,25 @@
+package com.rcldevelop.moremekanismores.util;
+
+import com.rcldevelop.moremekanismores.MoreMekanismOres;
+import com.rcldevelop.moremekanismores.items.ItemBase;
+import net.minecraft.item.Item;
+import net.minecraft.item.Items;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class RegistryHandler {
+
+    public static final DeferredRegister<Item> ITEMS = new DeferredRegister<>(ForgeRegistries.ITEMS, MoreMekanismOres.MOD_ID);
+
+    public static void init(){
+        ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
+    }
+
+    //Items
+    public static final RegistryObject<Item> VULCANITE_CLUMP = ITEMS.register("clump_vulcanite", ItemBase::new);
+    public static final RegistryObject<Item> VULCANITE_DIRTY_DUST = ITEMS.register("dirty_dust_vulcanite", ItemBase::new);
+    public static final RegistryObject<Item> VULCANITE_DUST = ITEMS.register("dust_vulcanite", ItemBase::new);
+
+}
